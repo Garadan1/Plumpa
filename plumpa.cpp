71443faa01c876a9fdccb818719c0954cd0266ef
garadan <garadan@umich.edu>
2020-12-26 10:12:46 -0500

    Created plumpa.cpp with includes and main

---
+++ plumpa.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <ctime> // For message timestamps
+#include "plumpa.h"
+
+
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+	Plumpa p;
+	return 0;
+}
